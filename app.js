d52629f5848ba4e2715a4e4ed795e171fcdb21af
Yoann <yoannpascalbouix@gmail.com>
2021-01-08 12:27:13 +0100

    Set game over wen touching walls

--- app.js
+++ app.js
@@ -1,50 +1,73 @@
 
-    let rowStart = 1
-    let columnStart = 1
-    let rowEnd = 2
-    let columnEnd = 2
+let rowStart = 2
+let columnStart = 2
+let rowEnd = 3
+let columnEnd = 3
+const snake = document.getElementById('snake')
+const gameOver = document.getElementById('game-over')
 
-    const history = [{rowStart: 1, columnStart: 1, rowEnd: 2, columnEnd: 2}]
+let keyHistory = []
+let lastDirection = 40
 
-    console.log(history)
+const getKeyCodeFromKeyTouch = (event) => {
+    keyHistory.push(event.keyCode)
+    let eventLastHistoryCode = keyHistory[keyHistory.length - 1]
+    lastDirection = eventLastHistoryCode
+    setDirectionFromKeyTouch(lastDirection)
+    console.log({rowStart, columnStart, rowEnd, columnEnd})
+    if (rowStart === 1 || columnStart === 1 || rowStart === 10 || columnStart === 10) {
+        snake.style.setProperty('background-color','yellow')
+        document.removeEventListener('keydown', getKeyCodeFromKeyTouch)
+        gameOver.style.setProperty('z-index','1')
+
+    } 
+}
+
+const setDirectionFromKeyTouch = (eventKeyCode) => {
     
-const moveSnake = (event) => {
-
-    const snake = document.getElementById('snake')
-
-    let direction = []
-    if (event.keyCode === 38) {
-        rowStart -= 1
-        rowEnd -= 1
-        return 'top'
-    } else if (event.keyCode === 39) {
-        columnStart += 1
-        columnEnd += 1
-        return 'right'
-    }  else if (event.keyCode === 40) {
-        rowStart += 1
-        rowEnd += 1
-        return 'down'
-    } else if (event.keyCode === 37) {
-        columnStart -= 1
-        columnEnd -= 1
-        return 'left'
-    } else if (rowEnd === 11) {
-        return 'game over'
+    if (eventKeyCode === 39) {
+        moveRight()
+    } else if (eventKeyCode === 40) {
+        moveDown()
+    } else if (eventKeyCode === 37) {
+        moveLeft()
+    } else if (eventKeyCode === 38) {
+        moveUp()
     }
+}
 
-
+const moveRight = () => {
+    columnStart += 1
+    columnEnd += 1
     snake.style.setProperty(`grid-area`,`${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd}`)
-    
+    console.log('right!')
+}
+
+const moveDown = () => {
+    rowStart += 1
+    rowEnd += 1
+    snake.style.setProperty(`grid-area`,`${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd}`)
+    console.log('down!')
+}
+
+const moveUp = () => {
+    rowStart -= 1
+    rowEnd -= 1
+    snake.style.setProperty(`grid-area`,`${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd}`)
+    console.log('up!')
+}
+
+const moveLeft = () => {
+    columnStart -= 1
+    columnEnd -= 1
+    snake.style.setProperty(`grid-area`,`${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd}`)
+    console.log('left!')
 }
 
 
-// setInterval(moveSnake, 1000)
-
-
-document.addEventListener('keydown', moveSnake);
-//setInterval(function(){ alert("Hello"); }, 1000);
 
 
 
+//setInterval(setDirectionFromKeyTouch(lastDirection), 1000) 
 
+document.addEventListener('keydown', getKeyCodeFromKeyTouch);
