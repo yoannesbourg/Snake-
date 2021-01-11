17e5436128f2b028e7701747d74212cfef41fb24
Yoann <yoannpascalbouix@gmail.com>
2021-01-11 19:37:14 +0100

    Generate random food

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
+let points = 0
+const counter = document.getElementById('counter')
 
+
+//Set grid size
+let gridColumnsMax = 32
+let gridColumnsPercent = 3.125
+const grid = document.getElementById('grid')
+grid.style.setProperty('grid-template-columns',`repeat(${gridColumnsMax}, ${gridColumnsPercent}%)`)
+grid.style.setProperty('grid-template-rows',`repeat(${gridColumnsMax}, ${gridColumnsPercent}%)`)
+
+//Snake Initial position
 let rowStart = 2
 let columnStart = 2
 let rowEnd = 3
 let columnEnd = 3
+
+//Food position
+let foodRowStart 
+let foodColumnStart
+let foodRowEnd 
+let foodColumnEnd 
+
+
+//General variables
 const snake = document.getElementById('snake')
+const food = document.getElementById('food')
 const gameOver = document.getElementById('game-over')
 snake.style.setProperty(`grid-area`,`${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd}`)
 
+let stopSetTimeOut 
+
+//History snake positions
 let keyHistory = []
 let lastDirection = 40
 
-const getKeyCodeFromKeyTouch = (event) => {
-    keyHistory.push(event.keyCode)
-    let eventLastHistoryCode = keyHistory[keyHistory.length - 1]
-    lastDirection = eventLastHistoryCode
-    setDirectionFromKeyTouch(lastDirection)
-    console.log({rowStart, columnStart, rowEnd, columnEnd})
-    if (rowStart === 1 || columnStart === 1 || rowStart === 10 || columnStart === 10) {
-        snake.style.setProperty('background-color','yellow')
-        document.removeEventListener('keydown', getKeyCodeFromKeyTouch)
-        gameOver.style.setProperty('z-index','1')
-
-    } 
-}
-
+//Get direction with keyCode and call moving functions
 const setDirectionFromKeyTouch = (eventKeyCode) => {
     
     if (eventKeyCode === 39) {
@@ -37,66 +48,89 @@ const setDirectionFromKeyTouch = (eventKeyCode) => {
     }
 }
 
+
+//Moving functions
 const moveRight = () => {
     columnStart += 1
     columnEnd += 1
     snake.style.setProperty(`grid-area`,`${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd}`)
-    console.log('right!')
 }
 
 const moveDown = () => {
     rowStart += 1
     rowEnd += 1
     snake.style.setProperty(`grid-area`,`${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd}`)
-    console.log('down!')
 }
 
 const moveUp = () => {
     rowStart -= 1
     rowEnd -= 1
     snake.style.setProperty(`grid-area`,`${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd}`)
-    console.log('up!')
 }
 
 const moveLeft = () => {
     columnStart -= 1
     columnEnd -= 1
     snake.style.setProperty(`grid-area`,`${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd}`)
-    console.log('left!')
 }
 
-let stopSetTimeOut 
+
 
 const play = (event) => {
+    //Stop timeout for no cumulating movements
     clearTimeout(stopSetTimeOut)
+
+    //If event push keycode (for calling setdirection function) if not mantain last direction
     if(event) {
         keyHistory.push(event.keyCode)
     } else {
         keyHistory.push(lastDirection)
     }
-
     
-
-    let eventLastHistoryCode = keyHistory[keyHistory.length - 1]
-    lastDirection = eventLastHistoryCode
+    //Get last direction from history
+    lastDirection = keyHistory[keyHistory.length - 1]
+    
+    //Call setdirection function with 
     setDirectionFromKeyTouch(lastDirection)
-    console.log({rowStart, columnStart, rowEnd, columnEnd})
 
-    if (rowStart === 0 || columnStart === 0 || rowStart === 11 || columnStart === 11) {
+    //If exciding the limit
+    if (rowStart === 0 || columnStart === 0 || rowStart === gridColumnsMax + 1 || columnStart === gridColumnsMax + 1) {
         gameOverFunction()
     } else {
-        stopSetTimeOut = setTimeout(play, 1000)
+        stopSetTimeOut = setTimeout(play, 500)
     }
   
 }
 
+//Game over
 const gameOverFunction = () => {
     snake.style.setProperty('background-color','yellow')
     document.removeEventListener('keydown', play)
     gameOver.style.setProperty('z-index','1')
 }
 
+//Generate food in random position
+const generateRandomFoodCoordonates = () => {
+    foodRowStart = Math.floor(Math.random() * gridColumnsMax + 1)
+    foodColumnStart =  Math.floor(Math.random() * gridColumnsMax + 1)
+    foodRowEnd =  foodRowStart + 1
+    foodColumnEnd =  foodColumnStart + 1
+    food.style.setProperty(`grid-area`,`${foodRowStart} / ${foodColumnStart} / ${foodRowEnd} / ${foodColumnEnd}`)
+}
 
-//setInterval(setDirectionFromKeyTouch(lastDirection), 1000) 
+//Snake eats food
+const raiseCounter = () =>{
+        counter.innerHTML = points += 1
+}
 
 document.addEventListener('keydown', play);
+document.addEventListener('click', raiseCounter);
+
+const generateFood = () => {
+    generateRandomFoodCoordonates()
+    
+    if (rowStart === foodRowStart && columnStart === foodColumnStart) {
+        console.log('ate')
+    }
+}
+generateFood()
